eda4d1684dcfc57184681ab8d29423221d7441b4
Abdullah Al Nahdi <al.nahdi.tnmy@gmail.com>
2020-10-05 09:44:34 +0600

    Recat Hooks- using prevState in useState

--- src/components/HookCounter.js
+++ src/components/HookCounter.js
@@ -3,10 +3,16 @@ import React, {useState} from 'react'
 const HookCounter = () => {
 
     const [state, setstate] = useState(0)
+    const increamentTen = () => {
+        for (let i = 0; i < 10; i++) {
+            setstate(prevState => prevState+1)
+            
+        }
+    }
     return (
         <div>
             <h1> Value: {state} </h1>
-            <button onClick={() => setstate(state +1)}>Increament</button>
+            <button onClick={() => increamentTen()}>Increament</button>
         </div>
     )
 }
