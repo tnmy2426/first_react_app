bd0423af202c1879eed542b97ab24f7eafd4959d
Abdullah Al Nahdi <al.nahdi.tnmy@gmail.com>
2020-10-05 13:01:55 +0600

    React Hook- conditionally run useEffect hook for a single value

--- src/components/HookCounterTwo.js
+++ src/components/HookCounterTwo.js
@@ -2,12 +2,16 @@ import React, {useState, useEffect} from 'react'
 
 const HookCounterTwo = () => {
     const [count, setcount] = useState(0)
+    const [name, setname] = useState("")
 
     useEffect(() => {
+        console.log("Use effect runs")
         document.title = `Count ${count}`
-    })
+    },[count])
     return (
         <div>
+            <input type='text' value={name} onChange= {e => setname(e.target.value) } />
+            <h3>Name: {name}</h3>
             <h1>Count: {count}</h1>
             <button onClick= {() => setcount(count + 1)}>Increament</button>
         </div>
