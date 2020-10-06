937dc95570441abd6699777c15fb4b96d1cf56eb
Abdullah Al Nahdi <al.nahdi.tnmy@gmail.com>
2020-10-06 11:39:39 +0600

    React Hook- useContext() hook using for data passing (previous example used )

--- src/components/ComponentY.js
+++ src/components/ComponentY.js
@@ -1,35 +1,18 @@
-import React, { Component } from 'react'
+import React, { useContext } from 'react'
 import { ProfileContext, UserContext } from '../App'
 import ComponentZ from './ComponentZ'
 
-class ComponentY extends Component {
-    render() {
-        return (
-            <div>
-                <ComponentZ />
-                <UserContext.Consumer>
-                    {
-                        name => {
-                            return (
-                                <ProfileContext.Consumer>
-                                    {
-                                        profile => {
-                                            return (
-                                                <div>
-                                                    <h1>Name:{name}</h1>
-                                                    <h3>Profile: {profile}</h3>
-                                                </div>
-                                            )
-                                        }
-                                    }
-                                </ProfileContext.Consumer>
-                            )
-                        }
-                    }
-                </UserContext.Consumer>
-            </div>
-        )
-    }
+const ComponentY = () => {
+
+    const name = useContext(UserContext)
+    const profile = useContext(ProfileContext)
+    return (
+        <div>
+            <ComponentZ />
+            <h1>Name:{name}</h1>
+            <h3>Profile:{profile}</h3>
+        </div>
+    )
 }
 
 export default ComponentY
